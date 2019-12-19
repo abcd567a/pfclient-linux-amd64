0c8dd893a6acb6f0cbcb8a2daf1ebf1362d5b321
abcd567a <abcd567@hotmail.com>
2019-12-19 14:47:43 -0500

    Update install-pfclient-new.sh

--- install-pfclient-new.sh
+++ install-pfclient-new.sh
@@ -30,7 +30,7 @@ sudo touch ${SERVICE_FILE}
 sudo chmod 777 ${SERVICE_FILE}
 /bin/cat <<EOM >${SERVICE_FILE}
 # planefinder uploader service for systemd
-# install in /lib/systemd/system
+# install in /lib/systemd/system/
 
 [Unit]
 Description=Planefinder Feeder
@@ -38,8 +38,7 @@ After=network-online.target
 
 [Service]
 Type=simple
-Restart=always
-LimitCORE=infinity
+SyslogIdentifier=pfclient
 RuntimeDirectory=pfclient
 RuntimeDirectoryMode=0755
 User=pfc
@@ -48,8 +47,6 @@ ExecStartPre=-/bin/chown pfc /var/log/pfclient
 ExecStart=/usr/bin/pfclient --config_path=/etc/pfclient-config.json --log_path=/var/log/pfclient $ 2>/var/log/pfclient/error.log
 Restart=on-failure
 RestartSec=30
-PermissionsStartOnly=true
-StandardOutput=null
 
 [Install]
 WantedBy=multi-user.target
